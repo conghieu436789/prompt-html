920bbafc257c4a732a690b4e2ff64ac41af41b1a
conghieu436789 <nguyenconghieu17494@gmail.com>
2021-02-24 16:18:52 +0700

    average grade

---
+++ grade.js
@@ -0,0 +1,16 @@
+let phys = prompt("enter the grade")
+let phys1= Number(phys);
+Number('phys');
+let chemistry = prompt("enter the grade")
+let chemistry1= Number(chemistry);
+Number('chemistry');
+let biology = prompt("enter the grade")
+let biology1= Number(biology);
+
+let sum=phys1 + chemistry1 + biology1
+let avg=sum/3
+document.write("The total is:")
+document.write(sum)
+document.write('<br>')
+document.write('Average is:')
+document.write(avg)
